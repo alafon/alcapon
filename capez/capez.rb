c4d3111a6a62ca91756ee048ead95badf0725ae5
Arnaud Lafon <arnaud.lafon@gmail.com>
2011-12-15 15:55:55 +0100

    Added a checking task to see if there are some change in the local repo

--- capez/capez.rb
+++ capez/capez.rb
@@ -16,6 +16,7 @@ after "deploy:update", :roles => :web do
 end
 
 before "deploy", :roles => :web do
+  capez.dev.local_check
   deploy.web.disable
 end
 
@@ -99,4 +100,75 @@ namespace :capez do
     end
   end
   # End of namespace :capez:autoloads
+
+  # Should be transformed in a simple function (not aimed to be called as a Cap task...)
+  namespace :dev do
+    desc <<-DESC
+      Checks if there are local changes or not (only with Git)
+      Considers that your main git repo is at the top of your eZ Publish install
+      If it is the case, ask the user to continue or not
+    DESC
+    task :local_check do
+      if "#{scm}" != "git" then
+        abort "Feature only available with git"
+      end
+
+      # TODO should be configurable
+      ezroot_path = "#{File.dirname(__FILE__)}/../.."
+
+      git_status = git_status_result( ezroot_path )
+
+      ask_to_abort = false
+      puts "Checking your local git..."
+      if git_status['has_local_changes']
+        ask_to_abort = true
+        puts "You have local changes"
+      end
+      if git_status['has_new_files']
+        ask_to_abort = true
+        puts "You have new files"
+      end
+
+      if ask_to_abort
+        user_abort = Capistrano::CLI.ui.ask "Abort ? y/n (n)"
+        abort "Deployment aborted to commit/add local changes" unless user_abort == "n" or user_abort == ""
+      end
+
+      if git_status['tracked_branch_status'] == 'ahead'
+        puts "You have #{git_status['tracked_branch_commits']} commits that need to be pushed"
+        push_before = Capistrano::CLI.ui.ask "Push them before deployment ? y/n (y)"
+        if push_before == "" or push_before == "y"
+          system "cd #{ezroot_path} && git push"
+        end
+      end
+    end
+  end
+
+  def git_status_result(path)
+    result = Hash.new
+    result['has_local_changes'] = false
+    result['has_new_files'] = false
+    result['tracked_branch'] = nil
+    result['tracked_branch_status'] = nil
+    result['tracked_branch_commits'] = 0
+    cmd_result = `cd #{path} && git status 2> /dev/null`
+    result['raw_result'] = cmd_result
+    cmd_result_array = cmd_result.split( /\n/ );
+    cmd_result_array.each { |value|
+      case value
+        when /# Changes not staged for commit:/
+          result['has_local_changes'] = true
+        when /# Untracked files:/
+          result['has_new_files'] = true
+        when /# On branch (.*)$/
+          result['branch'] = $1
+        when /# Your branch is (.*) of '(.*)' by (.*) commits?/
+          result['tracked_branch_status'] = $1
+          result['tracked_branch'] = $2
+          result['tracked_branch_commits'] = $3
+      end
+      }
+      return result
+  end
+
 end
