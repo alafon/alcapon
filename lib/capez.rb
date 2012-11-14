83d2a9361bf72b905623e81b48e6b4d2bc1cf055
Arnaud Lafon <arnaud.lafon@gmail.com>
2012-11-14 18:45:54 +0100

    Makes is possible to force group permissions on shared_children elements

--- lib/capez.rb
+++ lib/capez.rb
@@ -39,6 +39,9 @@ before "deploy:finalize_update" do
 end
 
 after "deploy:finalize_update" do
+  if fetch( :shared_children_group, false )
+    shared_children.map { |d| run( "chgrp -R #{shared_children_group} #{shared_path}/#{d.split('/').last}") }
+  end
   capez.var.init_release
   capez.var.link
   capez.settings.deploy
