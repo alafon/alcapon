fe546333d354b1ebcda97a00eefade0fdcbb5a36
Arnaud Lafon <arnaud.lafon@gmail.com>
2012-12-03 16:47:39 +0100

    Support for assets management in eZ Publish 5

--- lib/ezpublish4.rb
+++ lib/ezpublish4.rb
@@ -1,3 +1,14 @@
 puts "Running AlCapON for eZ Publish 4"
 
 set :ezp_legacy, ""
+
+after "deploy:finalize_update" do
+  if fetch( :shared_children_group, false )
+    shared_children.map { |d| run( "chgrp -R #{shared_children_group} #{shared_path}/#{d.split('/').last}") }
+  end
+  capez.var.init_release
+  capez.var.link
+  capez.settings.deploy
+  capez.autoloads.generate
+  #capez.cache.clear
+end
--- lib/ezpublish5.rb
+++ lib/ezpublish5.rb
@@ -2,3 +2,29 @@ puts "Running AlCapON for eZ Publish 5"
 
 set :ezp_legacy, "ezpublish_legacy"
 set :ezp_app, "ezpublish"
+
+after "deploy:finalize_update" do
+  if fetch( :shared_children_group, false )
+    shared_children.map { |d| run( "chgrp -R #{shared_children_group} #{shared_path}/#{d.split('/').last}") }
+  end
+  capez.var.init_release
+  capez.var.link
+  capez.settings.deploy
+  capez.autoloads.generate
+  capez.assets.install
+end
+
+namespace :capez do
+
+  namespace :assets do
+
+    task :install do
+
+      run( "php ezpublish/console assets:install --symlink #{fetch('ezp5_assets_path','web')}", :as => webserver_user )
+      run( "php ezpublish/console ezpublish:legacy:assets_install --symlink #{fetch('ezp5_assets_path','web')}", :as => webserver_user )
+
+    end
+
+  end
+
+end
