b6b5dcb695c46f8b99e968b015e2b42d65033b08
Arnaud Lafon <arnaud.lafon@gmail.com>
2014-05-16 19:33:34 +0200

    configuration point to enable cache clear (ezp4 only)

--- lib/ezpublish4.rb
+++ lib/ezpublish4.rb
@@ -10,7 +10,8 @@ after "deploy:finalize_update" do
   ezpublish.var.link
   ezpublish.settings.deploy
   ezpublish.autoloads.generate
-  #ezpublish.cache.clear
+  if fetch( :ezp4_trigger_cache_clear, false )
+    ezpublish.cache.clear
 end
 
 namespace :ezpublish do
